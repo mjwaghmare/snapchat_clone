3c3117f74854686fb72353d90b56fdfab2116c9d
manoj.waghmare@apptware.com <manoj.waghmare@apptware.com>
2021-10-17 17:05:14 +0530

    added splash screen & app icon

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const Color primary = Color(0xFFe0c600);
+const Color primary = Color(0xFFFFFC01);
 const Color white = Color(0xFFFFFFFF);
 const Color black = Color(0xFF000000);
 const Color darkGrey = Color(0xFF6e717b);
